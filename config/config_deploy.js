714d65abf2dda9f76b62454c6cbd42470135df65
nnaik39 <nanni.naik@gmail.com>
2023-09-05 14:29:27 -0700

    updated study config to debug mode

--- config/config_deploy.js
+++ config/config_deploy.js
@@ -12,7 +12,7 @@ var config_deploy = {
     // set deployment method; use one of: sample comment here
     //'debug', 'localServer', 'MTurk', 
     // 'MTurkSandbox', 'Prolific', 'directLink'
-    "deployMethod": "Prolific",
+    "deployMethod": "deploy",
     // "deployMethod": "directLink",
     // "deployMethod": "MTurk",
     // "deployMethod": "MTurkSandbox",
